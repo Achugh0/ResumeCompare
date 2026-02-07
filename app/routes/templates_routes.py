0ee4f74db18f6896fe99829089bc90647cc8199d
Achugh0 <137131753+Achugh0@users.noreply.github.com>
2026-02-07 19:11:37 +0530

    Update templates_routes.py

---
+++ app/routes/templates_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, send_from_directory, current_app
+
+templates_bp = Blueprint("templates_bp", __name__, url_prefix="/templates")
+
+@templates_bp.route("/resume")
+def download_resume_template():
+    folder = current_app.config["DOWNLOADS_FOLDER"]
+    return send_from_directory(folder, "resume_template.docx", as_attachment=True)
+
+@templates_bp.route("/job-description")
+def download_jd_template():
+    folder = current_app.config["DOWNLOADS_FOLDER"]
+    return send_from_directory(folder, "job_description_template.docx", as_attachment=True)
