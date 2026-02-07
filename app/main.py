ce3c8d18ad3114556c8811e049c4275c614e1a47
Achugh0 <137131753+Achugh0@users.noreply.github.com>
2026-02-07 19:33:07 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,4 +3,7 @@ from . import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # host="0.0.0.0" makes it accessible on localhost
+    # port=8000 changes from 5000 to 8000
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
